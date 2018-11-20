4ad7042625239e9d3485b06fec2c7b9bf69b781d
Tilly <tillyrwoodfield@gmail.com>
2018-11-20 10:47:16 +0000

    hello world web page

--- src/main/java/com/serendipity/gameController/control/MainController.java
+++ src/main/java/com/serendipity/gameController/control/MainController.java
@@ -2,6 +2,7 @@ package com.serendipity.gameController.control;
 
 import com.serendipity.gameController.model.Player;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,9 +13,11 @@ import java.util.List;
 public class MainController {
 
     @GetMapping(value="/")
-    @ResponseBody
-    public String home() {
-        return "Hello World!";
+    public String home(Model model) {
+        String name = "Tilly";
+        model.addAttribute("name", name);
+//        return "index.html";
+        return "temp";
     }
 
     public List<Player> initGame(){
