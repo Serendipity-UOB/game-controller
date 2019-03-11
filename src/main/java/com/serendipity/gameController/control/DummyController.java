8316ffef90830d99636658d084bd635d677261ef
jj16791 <jj16791@my.bristol.ac.uk>
2019-03-11 14:35:39 +0000

    update start info dummy response

--- src/main/java/com/serendipity/gameController/control/DummyController.java
+++ src/main/java/com/serendipity/gameController/control/DummyController.java
@@ -56,9 +56,9 @@ public class DummyController {
     @RequestMapping(value="/startInfoTest", method=RequestMethod.POST, consumes="application/json")
     @ResponseBody
     public ResponseEntity<String> getStartInfo(@RequestBody String json) {
-        String allPlayers = "{ \"all_players\": [{ \"id\": 2, \"real_name\": \"jack jones\", \"code_name\": \"CutieKitten\"}, " +
+        String allPlayers = "[{ \"id\": 2, \"real_name\": \"jack jones\", \"code_name\": \"CutieKitten\"}, " +
                 "{ \"id\": 3, \"real_name\": \"tilly woodfield\", \"code_name\": \"PuppyLover\"}, " +
-                "{ \"id\": 4, \"real_name\": \"tom walker\", \"code_name\": \"Cookingking\"} ]}";
+                "{ \"id\": 4, \"real_name\": \"tom walker\", \"code_name\": \"Cookingking\"}]";
         JSONObject output = new JSONObject();
         output.put("all_players", allPlayers);
         output.put("end_time", LocalTime.now().plusMinutes(1));
