775664161150d7b8be5b0f87b57948b8682b5dc3
jj16791 <jj16791@my.bristol.ac.uk>
2019-03-20 14:54:20 +0000

    basic get request for spectator

---
+++ src/main/java/com/serendipity/gameController/control/SpectatorController.java
@@ -0,0 +1,24 @@
+package com.serendipity.gameController.control;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpectatorController {
+
+    @GetMapping(value="/spectator")
+    public ResponseEntity<String> hello() {
+        // Create JSON object for response body
+        JSONObject output = new JSONObject();
+        // set default response status
+        HttpStatus responseStatus = HttpStatus.OK;
+
+        output.put("message", "hello");
+
+        return new ResponseEntity<>(output.toString(), responseStatus);
+    }
+
+}
