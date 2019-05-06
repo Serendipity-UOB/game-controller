958ea68e96eecd9eca02a83b0a808b715f1ac914
tillywoodfield <tillyrwoodfield@gmail.com>
2019-05-06 13:50:20 +0100

    Added script for copying logs

---
+++ copyLogs.sh
@@ -0,0 +1,13 @@
+heroku ps:copy registerPlayer.csv
+heroku ps:copy gameInfo.csv
+heroku ps:copy joinGame.csv
+heroku ps:copy startInfo.csv
+heroku ps:copy atHomeBeacon.csv
+heroku ps:copy playerUpdate.csv
+heroku ps:copy newTarget.csv
+heroku ps:copy exchangeRequest.csv
+heroku ps:copy exchangeResponse.csv
+heroku ps:copy expose.csv
+heroku ps:copy intercept.csv
+heroku ps:copy missionUpdate.csv
+heroku ps:copy endInfo.csv
